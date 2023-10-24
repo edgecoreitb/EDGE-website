2c4ee91dfecbf229edf24c22e9392e56cb64676a
Muhammad Tito Prakasa <muhammadtitoprks@Muhammads-MacBook-Pro.local>
2023-10-24 13:45:47 +0700

    init navbar

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans_Condensed } from "next/font/google";
+import "./globals.css";
+
+const DEFAULT_FONT = IBM_Plex_Sans_Condensed({ weight: "400", subsets: ["latin"]  });
+
+export const metadata: Metadata = {
+  title: "EDGE Core ITB",
+  description: "Website created by EDGE Core ITB",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={DEFAULT_FONT.className}>{children}</body>
+    </html>
+  );
+}
