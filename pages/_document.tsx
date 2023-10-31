596c5b5d8fac13d84fb5b017409441689e2c39f5
Muhammad Tito Prakasa <cocabc1@gmail.com>
2023-10-31 13:10:43 +0700

    chore: add icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
